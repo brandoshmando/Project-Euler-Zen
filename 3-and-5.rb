7f33ee0a619bdb2d97bdc3f44c99b8d90f7fe3a4
brandoshmando <brancraft@gmail.com>
2014-09-14 11:36:30 -0400

    Made the method more felixible with ability to add max number. Also added comment

--- 3-and-5.rb
+++ 3-and-5.rb
@@ -1,4 +1,5 @@
-def multiples
-  multiples = (1...1000).select {|i| i % 3 == 0 || i % 5 == 0 }
+#Returns a sum of all numbers that are multiples of 3 and 5 BELOW a given max number
+def multiples(max)
+  multiples = (1...max).select {|i| i % 3 == 0 || i % 5 == 0 }
   multiples.reduce(:+)
 end
